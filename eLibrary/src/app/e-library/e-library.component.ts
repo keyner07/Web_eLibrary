1d6500e6d9df8f3ee7ccab7e29fee4a12fec2623
MagicBackSlash <e.cabrera2993@gmail.com>
2019-11-30 15:09:23 -0500

    Parte visual de login, home, eLibrary completada

--- eLibrary/src/app/e-library/e-library.component.ts
+++ eLibrary/src/app/e-library/e-library.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-e-library',
-  templateUrl: './e-library.component.html',
-  styleUrls: ['./e-library.component.css']
+  selector: "app-e-library",
+  templateUrl: "./e-library.component.html",
+  styleUrls: ["./e-library.component.css"]
 })
 export class ELibraryComponent implements OnInit {
+  Arr = Array; //Array type captured in a variable
+  num: number = 20;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
